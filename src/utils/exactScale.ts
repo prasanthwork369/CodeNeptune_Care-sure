34b79c336772daadeae6965263c3a65faf80004a
prasanthwork369 <prasanth.work369@gmail.com>
2026-06-25 18:56:01 +0530

    Add worklet directive to exactScale to prevent UI thread crashes

--- src/utils/exactScale.ts
+++ src/utils/exactScale.ts
@@ -5,6 +5,7 @@ const { width } = Dimensions.get("window");
 const FIGMA_WIDTH = 390;
 
 export const exactScale = (size: number) => {
+  "worklet";
   const scale = width / FIGMA_WIDTH;
 
   return size * Math.min(scale, 1.15);
